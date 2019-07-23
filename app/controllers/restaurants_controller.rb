b568d363edb258b376882bb8bf5b9dd3a72c2df8
Sam Cheesman <s.cheesman1234500@gmail.com>
2019-07-23 20:22:52 +0100

    Created controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,40 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurant = Restaurant.all
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    # redirect_to restuarants_path
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit() end
+
+  def show() end
+
+  def update
+    @restaurant.update(restaurant_params)
+    # redirect_to restuarants_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    # redirect_to restuarants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :description, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top', to: 'restaurants#top'
+    end
+  end
 end
