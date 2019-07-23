1639431df28dfca149102d0adbfa478c9bbfc315
Sam Cheesman <s.cheesman1234500@gmail.com>
2019-07-23 20:53:38 +0100

    Added phone number and category

---
+++ db/migrate/20190723194154_add_phone_number_and_category_to_restaurants.rb
@@ -0,0 +1,6 @@
+class AddPhoneNumberAndCategoryToRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :restaurants, :phone_number, :string
+    add_column :restaurants, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_183030) do
+ActiveRecord::Schema.define(version: 2019_07_23_194154) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_07_23_183030) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "category"
   end
 
 end
